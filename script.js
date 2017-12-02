59977748fee5abd214a6e26147357170a386eb8b
zajacamadeusz <zajacamadeusz7@gmail.com>
2017-12-02 14:12:40 +0100

    modify

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'https://afternoon-chamber-90676.herokuapp.com/v1/task';
+  var apiRoot = 'https://afternoon-chamber-90676.herokuapp.com/v1/task/';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
