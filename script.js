5985ba808078f9444375ea10c696df8132862e83
zajacamadeusz <zajacamadeusz7@gmail.com>
2018-01-30 21:34:33 +0100

    new server for app

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'https://afternoon-chamber-90676.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'https://afternoon-chamber-90676.herokuapp.com/v1/trello/';
+  const apiRoot = 'https://task-crud-app-az.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://task-crud-app-az.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
