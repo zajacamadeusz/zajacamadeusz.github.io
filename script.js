fba06b0ee38296e527a3a44f1eabc809c02d6bff
zajacamadeusz <zajacamadeusz7@gmail.com>
2018-01-31 23:25:07 +0100

    m

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://obscure-chamber-62596.herokuapp.com/v1';
+  const apiRoot = 'https://obscure-chamber-62596.herokuapp.com/v1/';
   const trelloApiRoot = 'https://obscure-chamber-62596.herokuapp.com/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
