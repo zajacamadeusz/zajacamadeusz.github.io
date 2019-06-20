e9d3aa9b8cdc0685152c1d04497a4239b0a94380
Amadeusz Zajac <zajacamadeusz7@gmail.com>
2019-06-20 22:08:12 +0200

    error fixed

--- rest-springboot-app/script.js
+++ rest-springboot-app/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  const apiRoot = 'https://aqueous-coast-18578.herokuapp.com/v1';
+  const apiRoot = 'https://aqueous-coast-18578.herokuapp.com/v1/';
   const trelloApiRoot = 'https://aqueous-coast-18578.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
