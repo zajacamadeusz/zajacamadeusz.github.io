62cf1deaeff5774d94b3c38c4bc5d3efa90813c3
Amadeusz Zajac <zajacamadeusz7@gmail.com>
2019-06-20 22:03:40 +0200

    api url changed

--- rest-springboot-app/script.js
+++ rest-springboot-app/script.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
-  const apiRoot = 'https://obscure-chamber-62596.herokuapp.com/v1/';
-  const trelloApiRoot = 'https://obscure-chamber-62596.herokuapp.com/v1/trello/';
+$(document).ready(function () {
+  const apiRoot = 'https://aqueous-coast-18578.herokuapp.com/v1';
+  const trelloApiRoot = 'https://aqueous-coast-18578.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
@@ -18,7 +18,7 @@ $(document).ready(function() {
       url: requestUrl,
       method: 'GET',
       contentType: 'application/json',
-      success: function(boards) { callback(callbackArgs, boards); }
+      success: function (boards) { callback(callbackArgs, boards); }
     });
   }
 
@@ -36,11 +36,11 @@ $(document).ready(function() {
   }
 
   function prepareBoardOrListSelectOptions(availableChoices) {
-    return availableChoices.map(function(choice) {
+    return availableChoices.map(function (choice) {
       return $('<option>')
-                .addClass('crud-select__option')
-                .val(choice.id)
-                .text(choice.name || 'Unknown name');
+        .addClass('crud-select__option')
+        .val(choice.id)
+        .text(choice.name || 'Unknown name');
     });
   }
 
@@ -50,7 +50,7 @@ $(document).ready(function() {
       availableBoards[board.id] = board;
     });
 
-    taskData.forEach(function(task) {
+    taskData.forEach(function (task) {
       var $datatableRowEl = createElement(task);
       var $availableBoardsOptionElements = prepareBoardOrListSelectOptions(boards);
 
@@ -69,7 +69,7 @@ $(document).ready(function() {
       url: requestUrl,
       method: 'GET',
       contentType: "application/json",
-      success: function(tasks) {
+      success: function (tasks) {
         tasks.forEach(task => {
           availableTasks[task.id] = task;
         });
@@ -97,7 +97,7 @@ $(document).ready(function() {
         title: taskTitle,
         content: taskContent
       }),
-      success: function(data) {
+      success: function (data) {
         parentEl.attr('data-task-id', data.id).toggleClass('datatable__row--editing');
         parentEl.find('[data-task-name-paragraph]').text(taskTitle);
         parentEl.find('[data-task-content-paragraph]').text(taskContent);
@@ -113,8 +113,8 @@ $(document).ready(function() {
     $.ajax({
       url: requestUrl,
       method: 'DELETE',
-      success: function() {
-        parentEl.slideUp(400, function() { parentEl.remove(); });
+      success: function () {
+        parentEl.slideUp(400, function () { parentEl.remove(); });
       }
     })
   }
@@ -184,7 +184,7 @@ $(document).ready(function() {
         description: relatedTask.content,
         listId: selectedListId
       }),
-      success: function(data) {
+      success: function (data) {
         console.log('Card created - ' + data.shortUrl);
         alert('Card created - ' + data.shortUrl);
       }
@@ -193,10 +193,10 @@ $(document).ready(function() {
 
   $('[data-task-add-form]').on('submit', handleTaskSubmitRequest);
 
-  $tasksContainer.on('change','[data-board-name-select]', handleBoardNameSelect);
-  $tasksContainer.on('click','[data-trello-card-creation-trigger]', handleCardCreationRequest);
-  $tasksContainer.on('click','[data-task-edit-button]', toggleEditingState);
-  $tasksContainer.on('click','[data-task-edit-abort-button]', toggleEditingState);
-  $tasksContainer.on('click','[data-task-submit-update-button]', handleTaskUpdateRequest);
-  $tasksContainer.on('click','[data-task-delete-button]', handleTaskDeleteRequest);
+  $tasksContainer.on('change', '[data-board-name-select]', handleBoardNameSelect);
+  $tasksContainer.on('click', '[data-trello-card-creation-trigger]', handleCardCreationRequest);
+  $tasksContainer.on('click', '[data-task-edit-button]', toggleEditingState);
+  $tasksContainer.on('click', '[data-task-edit-abort-button]', toggleEditingState);
+  $tasksContainer.on('click', '[data-task-submit-update-button]', handleTaskUpdateRequest);
+  $tasksContainer.on('click', '[data-task-delete-button]', handleTaskDeleteRequest);
 });
